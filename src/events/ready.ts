529b1e459a94dd4b12c54dab58d654a894314d55
Greensky <david.hesliere@gmail.com>
2023-04-22 12:06:14 +0200

    feat: :sparkles: Add a client activity

---
+++ src/events/ready.ts
@@ -0,0 +1,9 @@
+import { AmethystEvent } from "amethystjs";
+import { ActivityType } from "discord.js";
+
+export default new AmethystEvent('ready', (client) => {
+    client.user.setActivity({
+        name: 'des calculs',
+        type: ActivityType.Watching
+    })
+})
